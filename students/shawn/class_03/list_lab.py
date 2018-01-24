6f8055951bab5d1bc32fd28d4e63113b9c129951
Shawn Hopkins <shopkins@seagen.com>
2018-01-23 21:18:42 -0800

    add the insert and if fruit starts with 'p' functionality

--- students/shawn/class_03/list_lab.py
+++ students/shawn/class_03/list_lab.py
@@ -4,22 +4,32 @@ def list_series1(list=["Apples","Pears","Oranges","Peaches"]):
 
     print(list)
 
-    more_fruit=input("Add some fruit > ")
+    # more_fruit=input("Add some fruit > ")
+    #
+    # if more_fruit:
+    #     list.append(more_fruit)
+    #
+    #     print(list)
+    #
+    # pos=0
+    # while not (1<= pos <len(list)+1 ):
+    #     pos = int(input("Enter fruit number > "))
+    #
+    #     if not (1<= pos <len(list)+1):
+    #         print("Numbers between 0 and {}".format(len(list)))
+    #
+    # else:
+    #     print("Position {} is  {}".format(pos, list[pos-1]))
 
-    if more_fruit:
-        list.append(more_fruit)
+    more_fruit = input("Prepend some more fruits > ")
+    while more_fruit:
+        more_fruit = input("Prepend some more fruits > ")
 
-        print(list)
-
-    pos=0
-    while not (1<= pos <len(list)+1 ):
-        pos = int(input("Enter fruit number > "))
-
-        if not (1<= pos <len(list)+1):
-            print("Numbers between 0 and {}".format(len(list)))
+        if more_fruit:
+            list.insert(0,more_fruit)
 
     else:
-        print("Position {} is  {}".format(pos, list[pos-1]))
+        print("Fruit list include: {}".format(list))
 
 
 
