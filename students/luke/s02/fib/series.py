9468145ada96c0e6070231f4bfa1ebae5662e768
Luke Rider <luke@rider.us>
2018-01-26 07:11:30 -0800

    Add tests.

--- students/luke/s02/fib/series.py
+++ students/luke/s02/fib/series.py
@@ -6,19 +6,24 @@ https://uwpce-pythoncert.github.io/PythonCertDevel/exercises/fib_and_lucas.html#
 Compute Fibonacci or Lucas sequence.
 """
 
-def fib(n, n_2=0, n_1=1):
+def series(n, n_2=0, n_1=1):
     """ Return n-th value in the Fibonacci/Lucas sequence using supplied terminal values. """
     if (n == 0):
         return n_2
     if (n == 1):
         return n_1
-    return (fib(n - 2) + fib(n - 1))
+    return (series(n - 2, n_2, n_1) + series(n - 1, n_2, n_1))
 
-print("First 10 Fibonacci numbers:")
-for i in range(10):
-    print(repr(i) + ": " + repr(fib(i, 0, 1)))
-print()
+# Test Fibonacci sequence
+assert(series(5) == 5)
+assert(series(9) == 34)
+print("Passed Fibonacci sequence tests.")
 
-print("First 10 Lucas numbers:")
-for i in range(10):
-    print(repr(i) + ": " + repr(fib(i, 2, 1)))
+# Test Lucas Sequence
+assert(series(5, 2, 1) == 11)
+assert(series(7, 2, 1) == 29)
+print("Passed Lucas sequence tests.")
+
+# Test defaults
+assert(series(25) == series(25, 0, 1))
+print("Passed default value tests.")
