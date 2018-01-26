14991aef242f6f2260875ccf01086dab70ac3094
ru57y34nn <mannrusty@gmail.com>
2018-01-25 22:06:35 -0800

    added make_grid function that prints grid by iterating throughmake_row

--- students/rusty_mann/Session02/grid_printer.py
+++ students/rusty_mann/Session02/grid_printer.py
@@ -1,10 +1,22 @@
 
 
-def make_row(char, middle = ' ', repeat = 2, length = 2):
+def make_row(char, middle = ' ', repeat = 4, length = 2):
     """create rows with "length" number of units and "repeat" size of units"""
     for k in range(length):
         print("%s%s" % (char, middle*repeat), end=' ')
     print(char)
 
-make_row('+', ' -')
-make_row('|', '  ')
+#make_row('+', ' -')
+#make_row('|', '  ')
+
+def make_grid(length = 2, repeat = 4):
+    """create grid with table height equal to length and cell height equal to repeat"""
+    for k in range(length):
+        make_row('+',' -', repeat, length)
+        for j in range(repeat):
+            make_row('|','  ',repeat, length)
+    make_row('+',' -', repeat, length)
+
+make_grid()
+make_grid(3,4)
+make_grid(5,3)
