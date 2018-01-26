8097589eb0ab21141fcdd822e238ab8ceae54a63
Luke Rider <luke@rider.us>
2018-01-25 19:19:42 -0800

    Genericize sequence function to handle either Fibanocci or Lucas sequence

--- students/luke/s02/fib/fibanocci.py
+++ students/luke/s02/fib/fibanocci.py
@@ -3,30 +3,22 @@
 """
 https://uwpce-pythoncert.github.io/PythonCertDevel/exercises/fib_and_lucas.html#exercise-fibonacci
 
-Compute Fibonacci sequence.
+Compute Fibonacci or Lucas sequence.
 """
 
-def fib(n):
-    """ Return n-th value in the Fibonacci sequence. """
+def fib(n, n_2=0, n_1=1):
+    """ Return n-th value in the Fibonacci/Lucas sequence using supplied terminal values. """
     if (n == 0):
-        return 0
+        return n_2
     if (n == 1):
-        return 1
+        return n_1
     return (fib(n - 2) + fib(n - 1))
-
-def lucas(n):
-    """ Return the n-th value in the Lucas sequence. """
-    if (n == 0):
-        return 2
-    if (n == 1):
-        return 1
-    return (lucas(n - 2) + lucas(n - 1))
 
 print("First 10 Fibonacci numbers:")
 for i in range(10):
-    print(repr(i) + ": " + repr(fib(i)))
+    print(repr(i) + ": " + repr(fib(i, 0, 1)))
 print()
 
 print("First 10 Lucas numbers:")
 for i in range(10):
-    print(repr(i) + ": " + repr(lucas(i)))
+    print(repr(i) + ": " + repr(fib(i, 2, 1)))
