a44e232c48c3c9df7333ee08812e661fb348370f
ru57y34nn <mannrusty@gmail.com>
2018-01-28 15:19:45 -0800

    added assert statements as test block

--- students/rusty_mann/Session02/series.py
+++ students/rusty_mann/Session02/series.py
@@ -9,7 +9,8 @@ def fibonacci(n):
     else:
         return fibonacci(n-2)+fibonacci(n-1)
 
-print(fibonacci(10))
+#print(fibonacci(10))
+######################################################################
 
 
 def lucas(n):
@@ -22,10 +23,13 @@ def lucas(n):
     else:
         return lucas(n-2)+lucas(n-1)
 
-print(lucas(10))
+#print(lucas(10))
+#####################################################################
 
 
 def sum_series(n, a=0, b=1):
+    """produce nth number in fibonacci sereis with default a, b arguments 
+         and lnth number in ucas series if a, b arguments are set to 2 and 1"""
     if n == 0:
         return 0
     if n == 1:
@@ -35,7 +39,13 @@ def sum_series(n, a=0, b=1):
     else:
         return sum_series(n-2,a,b)+sum_series(n-1,a,b)
 
+#print(sum_series(10))
+#print(sum_series(10,2,1))
 
-print(sum_series(10))
-print(sum_series(10,2,1))
+
+#testing, testing, 1,2,3
+assert fibonacci(5) == 3
+assert lucas(5) == 7
+assert sum_series(5) == 3
+assert sum_series(5,2,1) == 7
 
