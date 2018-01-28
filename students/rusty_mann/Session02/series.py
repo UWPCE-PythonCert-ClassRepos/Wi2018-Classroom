8ed8eb3671b95b46bf1a6271f107dc8fcb899939
ru57y34nn <mannrusty@gmail.com>
2018-01-28 15:05:12 -0800

    defined fibonacci function

---
+++ students/rusty_mann/Session02/series.py
@@ -0,0 +1,13 @@
+
+def fibonacci(n):
+    #if n == 0:
+    if n == 0 or n == 1:
+        return 0
+    #elif n == 1:
+    elif n == 2:
+        return 1
+    else:
+        return fibonacci(n-2)+fibonacci(n-1)
+
+print(fibonacci(10))
+
