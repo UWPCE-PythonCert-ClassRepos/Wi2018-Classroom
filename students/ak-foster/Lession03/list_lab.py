7c91bad408877945c820d9468c51052038715af4
Andrew Foster <akfoster@uw.edu>
2018-01-29 22:19:35 -0800

    Finished remaining series.

--- students/ak-foster/Lession03/list_lab.py
+++ students/ak-foster/Lession03/list_lab.py
@@ -1,38 +1,57 @@
 #!/usr/bin/env python3
-# Series 1
-a_list = ['Apples', 'Pears', 'Oranges', 'Peaches']
-print(a_list)
-response = 'Kiwi' #input('Please enter another fruit: ')
-a_list.append(response)
-print(a_list)
-response = '4' #input('Please enter a number: ')
-print(response + ' ' + a_list[(int(response) - 1)])
-a_list = ['Cherries'] + a_list
-a_list.insert(0, 'Bananas')
-for fruit in a_list:
-    if 'P' in fruit:
-        print(fruit, end=' ')
-# Series 2
-print(a_list)
-del a_list[-1]
-print(a_list)
-response = 'Pears'#input('Please enter a fruit to remove: ')
-try:
-    del a_list[a_list.index(response)]
-except:
-    print('Fruit not found.')
 
-# TODO: Bonus: Multiply the list times two. Keep asking until a match is found. Once found, delete all occurrences.
+m_list = ['Bananas', 'Cherries', 'Apples', 'Pears', 'Oranges', 'Peaches', 'Kiwi']
+
+def series1():
+    a_list = ['Apples', 'Pears', 'Oranges', 'Peaches']
+    print(a_list)
+    response = input('Please enter another fruit: ')
+    a_list.append(response)
+    print(a_list)
+    response = input('Please enter a number: ')
+    print(response + ' ' + a_list[(int(response) - 1)])
+    a_list = ['Cherries'] + a_list
+    a_list.insert(0, 'Bananas')
+    for fruit in a_list:
+        if 'P' in fruit:
+            print(fruit, end=' ')
+
+
+def series2():
+    a_list = list(m_list)
+    print(a_list)
+    del a_list[-1]
+    print(a_list)
+    response = input('Please enter a fruit to remove: ')
+    while True:
+        if response in a_list:
+            break
+        else:
+            response = input('Not found. Please enter a fruit to remove: ')
+    a_list = a_list * 2
+    for index, value in enumerate(a_list):
+        if value == response:
+            del a_list[index]
+    return a_list
 
 # Series 3
-for fruit in a_list:
-    rep = input('Do you like '+ fruit.lower() +'? ')
-    while (rep.lower() != 'yes') and (rep.lower() != 'no'):
-        rep = input('Please respond with \'yes\' or \'no\': ')
-    if rep.lower() == 'no':
-        del a_list[a_list.index(fruit)]
-print(a_list)
+def series3():
+    a_list = list(m_list)
+    for fruit in a_list:
+        rep = input('Do you like '+ fruit.lower() +'? ')
+        while (rep.lower() != 'yes') and (rep.lower() != 'no'):
+            rep = input('Please respond with \'yes\' or \'no\': ')
+        if rep.lower() == 'no':
+            del a_list[a_list.index(fruit)]
+    print(a_list)
 
 # Series 4
-"""Make a copy of the list and reverse the letters in each fruit in the copy.
-Delete the last item of the original list. Display the original list and the copy."""
+def series4():
+    a_list = []
+    global m_list
+    for i in m_list:
+        a_list.append(i[::-1])
+    del m_list[-1]
+    print(m_list)
+    print(a_list)
+
