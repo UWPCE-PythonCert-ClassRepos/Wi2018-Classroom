7c5fb50fbb56a8ba3943bf0baf37a1109c3d6b86
ru57y34nn <mannrusty@gmail.com>
2018-01-29 22:45:35 -0800

    added every_other function that skips every other char in sequence

--- students/rusty_mann/Session03/sequence_slicing.py
+++ students/rusty_mann/Session03/sequence_slicing.py
@@ -4,8 +4,14 @@ def switch_first_last(seq):
     new = seq[-1:] + mid + seq[:1]
     return new
 
-
 print(switch_first_last("this is a string"))
 print(switch_first_last([1,2,3,4,5,6,7,8,9,0]))
 print(switch_first_last((1,2,3,4,5,6,7,8,9,0)))
+
+
+def every_other(seq):
+    return seq[::2]
+
+print(every_other("this is a string"))
+print(every_other([1,2,3,4,5,6,7,8,9,0]))
 
