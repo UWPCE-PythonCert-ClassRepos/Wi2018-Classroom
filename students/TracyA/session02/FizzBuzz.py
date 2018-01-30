f7258e6d660081e8bbf51f19e5174b9b07180e24
Tracy A <tcpip201@gmail.com>
2018-01-29 22:02:00 -0800

    Committing the FizzBuzz.py test

---
+++ students/TracyA/session02/FizzBuzz.py
@@ -0,0 +1,16 @@
+# Programming in python B Winter 2018
+# January 26, 2017
+# FizzBuzz
+# Tracy Allen - git repo https://github.com/tenoverpar/Wi2018-Classroom
+
+#!/usr/bin/env python3
+
+for n in range(1, 101):
+    if n % 3 == 0 and n % 5 == 0:
+        print("FizzBuzz")
+    elif n % 3 == 0:
+        print("Fizz")
+    elif n % 5 == 0:
+        print("Buzz")
+    else:
+        print(n)
