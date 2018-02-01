fa84d5c07b6262df6d3a92a5df84841debebfa16
Jim Blackburn <jimbb82@uw.edu>
2018-01-31 20:35:41 -0800

    Fixed TypeError when trying to add elements to slices

--- students/jim/Lesson03/slicing.py
+++ students/jim/Lesson03/slicing.py
@@ -1,13 +1,14 @@
 my_string_1 = "camelot"
 my_string_2 = "spam"
+foo = [1, 2, 3, 4, 5]
 
 def exchange_first_last(seq):
-    new_seq = None
     try:
-        new_seq = seq[-1] + seq[1:-1] + seq[0]
+        new_seq = seq[-1:] + seq[1:-1] + seq[:1]
     except TypeError:
         print("Error: something went wrong.")
     return new_seq
 
 assert exchange_first_last(my_string_1) == "tameloc"
 assert exchange_first_last(my_string_2) == "mpas"
+assert exchange_first_last(foo) == [5, 2, 3, 4, 1]
