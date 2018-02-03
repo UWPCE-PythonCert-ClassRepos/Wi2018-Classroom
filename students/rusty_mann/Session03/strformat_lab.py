4fe26bcdd93d78b5fb141389e2905e6944665480
ru57y34nn <mannrusty@gmail.com>
2018-02-03 15:33:43 -0800

    re-did task two; pulled in numbers by tuple index

--- students/rusty_mann/Session03/strformat_lab.py
+++ students/rusty_mann/Session03/strformat_lab.py
@@ -5,7 +5,7 @@ format_string = "file_{:03}, {:.2f}, {:.2e}, {:.3G}"
 print(format_string.format(*nums))
 
 #Task Two
-f_strformat = f"file_{2:03}, {123.4567:.2f}, {10000:.2e}, {12345.67:.3G}"
+f_strformat = f"file_{nums[0]:03}, {nums[1]:.2f}, {nums[2]:.2e}, {nums[3]:.3G}"
 print(f_strformat)
 
 #Task Three
@@ -13,8 +13,16 @@ def form_string(*nums):
     n = len(nums)
     fstring = "The " +str(n) + " numbers are: " + "{:d}, "*(n-1) + "{:d}"
     return fstring.format(*nums)
-
-
 print(form_string(2,3,600,58,79,236,14))
 
+
+#Task Four
+date = (4,30,2017,2,27)
+stringy = "{3:02} {4} {2} {0:02} {1}"
+print(stringy.format(*date))
+
+
+#Task Five
+n = ["oranges", 1.3, "leomons", 1.1]
+print(f"The weight of an {(n[0])[0:6]}is {n[1]} and the weight of a {(n[2])[0:5]} is {n[3]}")
 
