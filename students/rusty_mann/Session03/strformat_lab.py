9f7c07bc79d9129cab36febcc3ca0f45adf183a0
ru57y34nn <mannrusty@gmail.com>
2018-02-03 00:45:46 -0800

    shortened form_string function to a couple of lines

--- students/rusty_mann/Session03/strformat_lab.py
+++ students/rusty_mann/Session03/strformat_lab.py
@@ -9,11 +9,12 @@ f_strformat = f"file_{2:03}, {123.4567:.2f}, {10000:.2e}, {12345.67:.3G}"
 print(f_strformat)
 
 #Task Three
-nums = (34, 56, 72, 98, 21)
-n= len(nums) 
-form_string1 = "the {:d} numbers are: "
-form_string2 = " {:d}, "*(len(nums)-1) + "{:d}"
-part1 = form_string1.format(n) 
-part2 = form_string2.format(*nums)
-print(part1+part2)
+def form_string(*nums):
+    n = len(nums)
+    fstring = "The " +str(n) + " numbers are: " + "{:d}, "*(n-1) + "{:d}"
+    return fstring.format(*nums)
+
+
+print(form_string(2,3,600,58,79,236,14))
+
 
