b15da71cba46340746a2fb863e4251125a47d442
ru57y34nn <mannrusty@gmail.com>
2018-02-04 02:15:38 -0800

    cleaned up script a little

--- students/rusty_mann/Session03/strformat_lab.py
+++ students/rusty_mann/Session03/strformat_lab.py
@@ -4,6 +4,7 @@ nums = (2, 123.4567, 10000, 12345.67)
 format_string = "file_{:03}, {:.2f}, {:.2e}, {:.3G}"
 print(format_string.format(*nums))
 
+
 #Task Two
 f_strformat = f"file_{nums[0]:03}, {nums[1]:.2f}, {nums[2]:.2e}, {nums[3]:.3G}"
 print(f_strformat)
@@ -23,6 +24,18 @@ print(stringy.format(*date))
 
 
 #Task Five
-n = ["oranges", 1.3, "leomons", 1.1]
+n = ["oranges", 1.3, "lemons", 1.1]
 print(f"The weight of an {(n[0])[0:6]}is {n[1]} and the weight of a {(n[2])[0:5]} is {n[3]}")
+
+
+#Task Six
+def make_table(cols):
+    '''cols is a sequence of three element sequences, first element is name, second is age, and third is cost'''
+    col_names = ["Name", "Age", "Cost"]
+    headers = f'{col_names[0]:13}{col_names[1]:3}{col_names[2]:^13}'
+    print(headers)
+    for n in range(len(cols)):
+        columns = f'{(cols[n])[0]:13}{(cols[n])[1]:0>3}{(cols[n])[2]:>13}'
+        print(columns)
+make_table([["Batman", 38, "$100,000.00"], ["Thor", 5, "$100.00"], ["Sponge Bob", 116, "$1,000.00"]])
 
