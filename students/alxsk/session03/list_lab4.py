cd8b80e9e7a18d7832e5a4a9f9372876b26dbe86
Alex Skrivanich <alexandria.is@gmail.com>
2018-02-04 18:56:03 -0800

    updating file

--- students/alxsk/session03/list_lab4.py
+++ students/alxsk/session03/list_lab4.py
@@ -1,6 +1,6 @@
 '''Series 1 of list lab exercises'''
 #Create a list that contains Apples, Pears, Oranges, and Peaches. 4 fruits. Print the list.
-print("Write a list of fruit. Then revers the letters of each fruit. Compare lists. ")
+print("Write a list of fruit. Then reverse the letters of each fruit. Compare lists. ")
 fruity=["Apples", "Pears", "Oranges", "Peaches"]
 print(fruity)
 
@@ -9,7 +9,9 @@ fruity_copy=fruity[:]
 
 #Reverse the letters in each fruit in the list copy. Print list.
 def reverse(f):
+	reversed_f = []
 	for fruit in fruity_copy:
-		return fruit[::-1]
+		reversed_f.append(fruit[::-1])
+	return reversed_f
 
 print(reverse(fruity_copy))
