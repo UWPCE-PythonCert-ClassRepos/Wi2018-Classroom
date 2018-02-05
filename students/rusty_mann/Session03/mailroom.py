fe1bf869562a24fb6bce18cd280c9c69e25d3b0a
ru57y34nn <mannrusty@gmail.com>
2018-02-04 22:26:52 -0800

    mailroom works but needs some cleaning up; break up larger functions into smaller ones

--- students/rusty_mann/Session03/mailroom.py
+++ students/rusty_mann/Session03/mailroom.py
@@ -5,20 +5,71 @@ donors = [["Allen, Paul", [1000000, 50000, 300000]],
                     ["Musk, Elon", [1000000, 30000]], 
                     ["Zuckerberg, Mark",[10000, 50000, 12000, 400000]]]
 
-donor_sort = sorted(donors, key=lambda donor: sum(donor[1]), reverse=True)
+#print(len(donors))
+#def sort_donors():
+
 #print(donor_sort)
 
+
 def make_report(donor):
+    donor_sort = sorted(donor, key=lambda donor: sum(donor[1]), reverse=True)
     col_names = ["Donor Name", "| Total Given", "| Num Gifts", "| Average Gift"]
     headers = f'{col_names[0]:20}{col_names[1]:>15}{col_names[2]:^15}{col_names[3]:20}'
     print(headers)
     print(("_")*65)
     print((" "))
-    for n in range(len(donors)):
-        columns = f'{(donor[n])[0]:20}{sum((donor[n])[1]):15}{len((donor[n])[1]):^15}{(sum((donor[n])[1])/len((donor[n])[1])):12.2f}'
+    for n in range(len(donor_sort)):
+        columns = f'{(donor[n])[0]:20}{sum((donor[n])[1]):15.2f}{len((donor[n])[1]):^15}{(sum((donor[n])[1])/len((donor[n])[1])):12.2f}'
         print(columns)
-make_report(donor_sort)
+    init_prompt()
+#make_report(donor_sort)
 
+
+def donation_amount(donor):
+    don_amt = input("Please enter a donation amount: ")
+    if don_amt == "quit":
+        init_prompt()
+    for n in range(len(donors)):
+        if (donors[n])[0] == donor:
+            (donors[n])[1].append(int(don_amt))
+            #return donors
+    #print(donors)
+    thank_you_email(donor, don_amt)
+    init_prompt()
+    #print(donors)
+
+
+def add_donor(new_donor):
+    donors.append(new_donor)
+    new_donor.append([])
+    #donation_amount()
+    #return donors
+    #print(donors)
+
+
+def choose_donor():
+    new_don = []
+    choose_don = input("Please enter a donor name in the form: Last_name, First_name: ")
+    if choose_don == "quit":
+        init_prompt()
+    for n in range(len(donors)):
+        if donors[n][0] == choose_don:
+            donation_amount(choose_don)
+            #break
+    else:
+        #if donors[n][0] != choose_don:
+        new_don.append(choose_don)
+        add_donor(new_don)
+                #new_don.append(choose_don)
+                #add_donor([choose_don])
+                #break
+    donation_amount(choose_don)
+    #new_don.append(choose_don)
+    #donors.append(new_don)
+    #new_don.append([])
+    #return donors
+    #print(donors)
+                #donation_amount(choose_don)
 
 def show_list():
     abc_sort = (sorted(donors, key=lambda donor: donor[0]))
@@ -26,18 +77,34 @@ def show_list():
         print((abc_sort[n])[0])
 
 
-def thank_you_email():
-    
+def thank_you_email(donor, amount):
+    print(f"Dear {donor}, thank you for your donation of {amount}. You are a good person")
 
-def init_prompt():
-    init_response = input("What do you want to do?")
-    if init_response == "list":
+
+def work_flow(response):
+    valid_response = ["1", "2", "3", "list"]
+    while response not in valid_response:
+        print("Please choose a valid response: ")
+        init_prompt()
+    if response == "list":
         show_list()
         init_prompt()
-    elif init_response == 1:
-        thank_you_email()
-    #return init_response
+    elif response == "1":
+        choose_donor()
+    elif response == "2":
+        make_report(donors)
+    else:
+        if response == "3":
+            quit()
+
+
+def init_prompt():
+    init_response = input("Would you like to, 1 Send a Thank You, 2 Create a Report, or 3 quit?")
+    work_flow(init_response)
+
 init_prompt()
+
+
 
 
 
