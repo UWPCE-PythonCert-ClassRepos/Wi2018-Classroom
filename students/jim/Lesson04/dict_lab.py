67849da8470a39b674cec632cbe58056480b4477
Jim Blackburn <jimbb82@uw.edu>
2018-02-07 20:02:29 -0800

    Completed 'Dictionaries 1' section of dict_lab.py

---
+++ students/jim/Lesson04/dict_lab.py
@@ -0,0 +1,16 @@
+# Dictionaries 1
+
+my_dict = {'name':'Chris', 'city':'Seattle', 'cake':'Chocolate'}
+print(my_dict)
+
+my_dict.pop('cake')
+
+print('cake' in my_dict)
+print(my_dict)
+
+my_dict['fruit'] = 'Mango'
+print(my_dict)
+print(my_dict.keys())
+print(my_dict.values())
+print('cake' in my_dict)
+print('Mango' in my_dict.values())
