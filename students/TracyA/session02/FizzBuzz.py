09aba811f5b752ae86ee90f9861081bd915ede04
Tracy A <tcpip201@gmail.com>
2018-02-08 18:37:06 -0800

    moved 'usr/bin/env python' to the top of the file

--- students/TracyA/session02/FizzBuzz.py
+++ students/TracyA/session02/FizzBuzz.py
@@ -1,9 +1,9 @@
+#!/usr/bin/env python
 # Programming in python B Winter 2018
 # January 26, 2017
 # FizzBuzz
 # Tracy Allen - git repo https://github.com/tenoverpar/Wi2018-Classroom
 
-#!/usr/bin/env python3
 
 for n in range(1, 101):
     if n % 3 == 0 and n % 5 == 0:
