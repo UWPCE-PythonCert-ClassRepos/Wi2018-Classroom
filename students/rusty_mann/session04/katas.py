f4441ac0360d768e2a25b63cea33d5be7af0bc1c
ru57y34nn <mannrusty@gmail.com>
2018-02-13 23:39:27 -0800

    almost there with katas.py.  It is running but won't end becasue I've told it to generate a new seed if new key does not match key in dict

--- students/rusty_mann/session04/katas.py
+++ students/rusty_mann/session04/katas.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import random
+
 
 trigram_dict = {}
 
@@ -8,7 +10,7 @@ def get_key(word):
             return akey
     return None
 
-with open('sherlock_small.txt') as f:
+with open('file_to_move.txt') as f:
     count = 0
     bad_chars = ',.-()'
     text = f.read()
@@ -17,14 +19,65 @@ with open('sherlock_small.txt') as f:
     words = text.split()
     for word in range(len(words)-2):
         k = words[word] + " " + words[word+1]
-        v = words[count+2]
+        v = words[word+2]
         akey = get_key(k)
         if akey is None:
             trigram_dict.setdefault(k, [])
         trigram_dict[k].append(v)
         count = count + 1
 
-print(trigram_dict)
+#print(trigram_dict)
+
+trigram_list = list(trigram_dict)
+#print(trigram_list)
+
+def gen_seed():
+    seed = random.choice(trigram_list)
+    return seed
+
+def get_rand_val(new_key):
+    for akey in trigram_dict:
+        if new_key == akey:
+            val = random.choice(trigram_dict[akey])
+            return val
+
+def get_last_two(new_str):
+    word_list = new_str.split()
+    new_key = word_list[-2] + " " + word_list[-1]
+    return new_key
+
+def get_new_word():
+    new_key = get_last_two(story_str)
+    if new_key is None:
+        new_key = gen_seed()
+    new_val = get_rand_val(new_key)
+    return str(new_val)
+
+story_str = " "
+print(story_str)
+seed_str = gen_seed()
+print(seed_str)
+story_str = story_str+seed_str
+print(story_str)
+#def make_new_story():
+    #story_str = story_str + get_new_word()
+    #return story_str
+
+#while True:
+new_word = get_new_word()
+story_str = story_str + " " + new_word
+print(story_str)
+#else:
+    #False
+    #make_new_story()
+
+    #new_str = new_str + get_rand_val(get_last_two(new_str))
+
+
+
+
+#with open('new_text', 'w') as f:
+    #f.write()
 
 """
 for key in dict:
