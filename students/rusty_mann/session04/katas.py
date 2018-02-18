c9ddcbdbc91614e1126d5d42d1405ca112922402
Rusty Mann <mannrusty@gmail.com>
2018-02-18 14:14:51 -0800

    Mostly working now, still working out a few bugs: still getting line endings on some words.  Stole a few ideas from solution.

--- students/rusty_mann/session04/katas.py
+++ students/rusty_mann/session04/katas.py
@@ -1,80 +1,100 @@
 #!/usr/bin/env python3
+
+
 import random
+import os
 
 
-trigram_dict = {}
+
+def read_in(filename):
+    with open(filename, 'r') as f:
+        for i in range(61):
+            f.readline()
+        text = []
+        for line in f:
+            if line.startswith("End of the Project Gutenberg EBook"):
+                break
+            text.append(line)
+    return " ".join(text)
 
 
-def get_key(word):
-    for akey in trigram_dict:
-        if word == akey:
-            return akey
-    return None
-
-with open('file_to_move.txt') as f:
-    count = 0
-    bad_chars = ',.-()'
-    text = f.read()
+def build_words(text):
+    bad_chars = ",.-(\)/"
+    #text = f.read()
     for c in bad_chars:
         text = text.replace(c, ' ')
+        text = text.lower()
     words = text.split()
+    for n, word in enumerate(words):
+        if word == 'i':
+            words[n] = "I"
+        #little_i = 'i'
+        #for i in little_i:
+            #word = word.replace(i, 'I')
+        #else:
+            #word
+    #for word in words:
+        #if word != "'":
+    return words
+
+
+def make_trigram(words):
+    trigram_dict = {}
+    #count = 0
     for word in range(len(words)-2):
-        k = words[word] + " " + words[word+1]
+        k = tuple(words[word:word + 2])
         v = words[word+2]
-        akey = get_key(k)
-        if akey is None:
-            trigram_dict.setdefault(k, [])
+        #akey = get_key(k, trigram_dict)
+        #if akey is None:
+        trigram_dict.setdefault(k, [])
         trigram_dict[k].append(v)
-        count = count + 1
+        #count = count + 1
+    return trigram_dict
 
-trigram_list = list(trigram_dict)
 
-def gen_seed():
+def gen_seed(tri_dict):
+    ''' '''
+    trigram_list = list(tri_dict)
     seed = random.choice(trigram_list)
     return seed
 
 
-story_str = ""
-seed_str = gen_seed()
-story_str = story_str+seed_str
-#print(story_str)
-#print(seed_str)
-#print(story_str)
+def get_rand_val(tri_dict, new_key):
+    val = random.choice(tri_dict[new_key])
+    return val
 
-
-def get_rand_val(new_key):
-    for akey in trigram_dict:
-        if new_key == akey:
-            val = random.choice(trigram_dict[akey])
-            return val
 
 def get_last_two(new_str):
-    word_list = new_str.split()
-    new_key = word_list[-2] + " " + word_list[-1]
+    new_key = tuple(new_str[-2:])
     return new_key
 
-def get_new_word(new_str):
-    new_key = get_last_two(new_str)
-    #if new_key is None:
-        #new_key = gen_seed()
-    new_val = get_rand_val(new_key)
+
+def get_new_word(tri_dict, new_key):
+    new_val = get_rand_val(tri_dict, new_key)
     return str(new_val)
 
 
+def make_new_story(tri_dict):
+    new_story = []
+    for i in range(20):
+        sentence = list(gen_seed(tri_dict))
+        for j in range(random.randint(2,15)):
+            new_pair = get_last_two(sentence)
+            sentence.append(get_new_word(tri_dict, new_pair))
+        sentence[0] = sentence[0].capitalize()
+        sentence[-1] += '.'
+        new_story.extend(sentence)
+    new_story = " ".join(new_story)
+    return new_story
 
-#def make_new_story():
-    #story_str = story_str + get_new_word()
-    #return story_str
-
-#def make_new_story(story_str):
-while True:
-    new_word = get_new_word(story_str)
-    if new_word != None:
-        story_str = '{:s} {:s}'.format(story_str, new_word)
+make_new_story
+    #new_word = get_new_word(story_str)
+    #if new_word != None:
+        #story_str = '{:s} {:s}'.format(story_str, new_word)
         #story_str = story_str + " " + new_word
-    else:
-        if new_word == None:
-            break
+    #else:
+        #if new_word == None:
+            #break
     #return story_str
 #make_new_story(story_str)
 
