45bbdd2211c8f77c86a228c16e86abb1fd31cf65
Navdeep Gill <navgill18@gmail.com>
2018-02-21 07:04:05 -0800

    exception exercise portion of lab

--- students/navdeep/session05/except_exercise.py
+++ students/navdeep/session05/except_exercise.py
@@ -17,33 +17,41 @@ first_try = ['spam', 'cheese', 'mr death']
 try:
     joke = fun(first_try[0])
 except:
-    print("hello")
-
+    print("Whoops! there is no joke for: {}".format(first_try[0]))
 # Here is a try/except block. Add an else that prints not_joke
 try:
     not_joke = fun(first_try[2])
 except SyntaxError:
     print('Run Away!')
 except NameError:
-    print("hello")
+    print("Hello!")
 else:
     print(not_joke)
+
 # What did that do? You can think of else in this context, as well as in
 # loops as meaning: "else if nothing went wrong"
 # (no breaks in  loops, no exceptions in try blocks)
 
+
+langs = ['java', 'c', 'python']
 # Figure out what the exception is, catch it and in that same block
 #
 # try calling the more_fun function with the 2nd language in the list,
 # again assigning it to next_joke.
-#
-# If there are no exceptions, call the more_fun function with the last
-# language in the list regardless of whether there was an exception
-
-# Finally, while still in the try/except block and regardless of whether
-# there were any exceptions, call the function last_fun with no
-# parameters. (pun intended)
-
-langs = ['java', 'c', 'python']
-
-more_joke = more_fun(langs[0])
+try:
+    next_joke = more_fun(langs[1])
+    #
+    # If there are no exceptions, call the more_fun function with the last
+    # language in the list regardless of whether there was an exception
+    more_fun(langs[2])
+except IndexError as e:
+    print("error: {}".format(e))
+finally:
+    # Finally, while still in the try/except block and regardless of whether
+    # there were any exceptions, call the function last_fun with no
+    # parameters. (pun intended)
+    last_fun()
+try:
+    more_joke = more_fun(langs[0])
+except IndexError:
+    print()
