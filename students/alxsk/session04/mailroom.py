56b8fb8430e15fe42b12063471a45e478ee28c99
Alex Skrivanich <alexandria.is@gmail.com>
2018-02-23 17:43:58 +0800

    mailroom V06

--- students/alxsk/session04/mailroom.py
+++ students/alxsk/session04/mailroom.py
@@ -21,34 +21,65 @@ donor_list= [('Alis Smith',500,350,400),
 ('Yash Kim',800)]
 
 def AddDonor():
-    pass
+    print("Current donor list:")
+    for row in donor_list:
+        print(row[0])
+        print()
+        '''
+        print(input("Add donor y/n: "))
+        if 'n':
+            print("no")
+        elif 'y':
+            print("yes")
+        else:
+            print("Error!")
+        '''
 
 def PrintReport():
 	#1. Create and empty list
     report=[]
 	#2. loop through donor_list computing values. Append new values to empty list using variables
-    for (name, amount) in report:
-        donation_count=len(amount)
-        total_donation=sum(amount)
-        avg_amount=total/donation_count
-        report.append(name,donation_count,total_donation,avg_amount)
-
-	#Create a layout fo the report, then pass variables into the report and print 
+    for row in donor_list:
+        new_row = []
+        name = row[0]
+        donation_count= len(row)-1
+        total_donation= sum(row[1:])
+        avg_amount= total_donation/donation_count
+        new_row.append(name)
+        new_row.append(donation_count)
+        new_row.append(total_donation)
+        new_row.append(avg_amount)
+        report.append(new_row)
+        # ***EDIT***Creates string. Instead make a multiple lists for each donor. 
+      
+	#3.Create a layout fo the report, then pass variables into the report and print 
     print("{:20s}|{:20s}|{:20s}|{:20s}".format("Name","Number of Donations","Total $ Donated","Average Donation Amount"))
     print("_"*80)
+    
+    print(report)
     for row in report:
-        print("{:20s}|{:20s}|{:20s}|{:20s}".format(row))
+    	#***EDIT***not working. needs to be iterable?
+        print("{:20s} {:20.2f} {:20d} {:20.2f} ".format(*row))
 
 def SendThankYou():
 	pass
 
-def crejhgjhj():
-	pass
+def main_menu_selection():
+#Mailroom user interface.
+    print("Welcome to the charity mailroom program!")
+    print("a - add new donor and charitable gift")
+    print("n - send thank you")
+    print("p - print donor donation report")
+    print("q - quit program")
+    selection=input("Pick a menu selection: ")  
+    print()
+    return selection.strip()
+
 
 #Iniate the program and define as a stand alone application.
 #Computer will store all of the previous functions and then use them here. 
 
-'''
+
 if __name__ == "__main__":
     running = True
     while running:
@@ -57,7 +88,7 @@ if __name__ == "__main__":
 #Handle the prompt input response from the main menu
         if prompt=='n':
             SendThankYou()
-        elif prompt=='r':
+        elif prompt=='p':
             PrintReport()
         elif prompt=='a':
             AddDonor()
@@ -65,4 +96,3 @@ if __name__ == "__main__":
             running=False
         else:
             print("Error! Selection invalid")
-'''
