0d7e65597d3ac2fb231d1c9e92c88a437981eea6
Alex Skrivanich <alexandria.is@gmail.com>
2018-02-24 12:08:33 -0800

    Everything mostly working

--- students/alxsk/session04/mailroom.py
+++ students/alxsk/session04/mailroom.py
@@ -10,36 +10,29 @@ The prompt handler responds to the different possible inputs from the prompt by 
 prompt handler.  
 '''
 
-#import packages math and dedent
 import math
 
 #First step. Donor List in global namespace. 
-donor_list= [('Alis Smith',500,350,400),
+DonorList= [('Alis Smith',500,350,400),
 ('Neha Johnson', 1000, 3452,431,333),
 ('Mohan Judge', 780,570),
 ('Sree Richards', 1500),
 ('Yash Kim',800)]
 
-def AddDonor():
-    print("Current donor list:")
-    for row in donor_list:
-        print(row[0])
-        print()
-        '''
-        print(input("Add donor y/n: "))
-        if 'n':
-            print("no")
-        elif 'y':
-            print("yes")
-        else:
-            print("Error!")
-        '''
+def FindOrAddDonor(EnterName):
+    for row in DonorList:
+        if EnterName.strip().lower() == row[0].lower():
+            return row
+    #Creating tuple of one item (the donor name)
+    DonorList.append((EnterName,))
+    return DonorList[-1]
+    
 
 def PrintReport():
 	#1. Create and empty list
     report=[]
 	#2. loop through donor_list computing values. Append new values to empty list using variables
-    for row in donor_list:
+    for row in DonorList:
         new_row = []
         name = row[0]
         donation_count= len(row)-1
@@ -52,22 +45,51 @@ def PrintReport():
         report.append(new_row) 
       
 	#3.Create a layout fo the report, then pass variables into the report and print 
-    print("{:20s}|{:20s}|  {:16s}|{:23s}|".format("Name","Number of Donations","Total $ Donated","Average Donation Amount"))
-    print("_"*83)
+    
+    print("{:20s}|{:^20s}|{:^20s}|{:23s}|".format("Name","Number of Donations","Total $ Donated","Average Donation Amount"))
+    print("_"*87)
     
     for row in report:
-        print("{:20s}|{:18.2f}  |{:16d}  |{:23.2f}| ".format(*row))
+        print("{:20s}|{:20.2f}|{:^20d}|{:^23.2f}| ".format(*row))
+    print()
     
-print()
+def Letter(name):
+    # The letter returned to general space
+    return 'Thank you {} for your generous donation!'.format(name)
 
 def SendThankYou():
-	pass
+    print("Current donor list:")
+    PrintReport()
+    
+         
+    EnterName = input("Type donor's full name, or enter new name, then press enter (q- main menu): ").strip().title()
+    # a name in the donorlist
+    if EnterName == "q":
+        return
+    # Now the donor name is stored in "EnterName"
 
-def main_menu_selection():
+    while True:
+        StrDonation=input("Enter donor's donation (0-to quit): ").strip()
+        NewDonation=float(StrDonation)
+        if NewDonation == 0:
+            return
+        else:
+            break
+
+    # Handle EnterName and NewDonation
+    donor = FindOrAddDonor(EnterName)
+    # Record the donation
+    # Note how the donor object can be manipulated while it is in the donors list.
+    donor[0].append(NewDonation)
+
+    print(Letter(donor))
+
+
+
+def MainMenu():
 #Mailroom user interface.
     print("Welcome to the charity mailroom program!")
-    print("a - add new donor and charitable gift")
-    print("n - send thank you")
+    print("n - add donation and send thank you")
     print("p - print donor donation report")
     print("q - quit program")
     selection=input("Pick a menu selection: ")  
@@ -82,15 +104,13 @@ def main_menu_selection():
 if __name__ == "__main__":
     running = True
     while running:
-        prompt = main_menu_selection()
+        prompt = MainMenu()
 
 #Handle the prompt input response from the main menu
         if prompt=='n':
             SendThankYou()
         elif prompt=='p':
             PrintReport()
-        elif prompt=='a':
-            AddDonor()
         elif prompt=='q':
             running=False
         else:
