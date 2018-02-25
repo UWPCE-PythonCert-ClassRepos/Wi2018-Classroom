45b8488a4cc2be6d29091076664b785207ed2a2b
ru57y34nn <mannrusty@gmail.com>
2018-02-24 23:00:59 -0800

    changed name of test file to test_mailroom.py and added test functions for testing donor_selection and get_donor_name functions.

--- students/rusty_mann/session06/mailroom_session06.py
+++ students/rusty_mann/session06/mailroom_session06.py
@@ -88,8 +88,12 @@ def add_donor(name, amount):
 
 
 def donor_selection():
-    name = input("Please enter a donor's name in the form of 'Last name, First name' "
-        "(or 'list' to see a list of all donors, or 'menu' to exit)> ").title()
+    #name = "mann, rusty".title()
+    #name = "allen paul".title()
+    #name = "Menu".title()
+    name = "LIST ".title()
+    #name = input("Please enter a donor's name in the form of 'Last name, First name' "
+    #"(or 'list' to see a list of all donors, or 'menu' to exit)> ").title()
     #print(name)
     return name
 
@@ -100,6 +104,7 @@ def get_donor_name():
         name = donor_selection()
         if name.strip().lower() == "list":
             show_donor_list()
+            break
         elif name.strip().lower() == "menu":
             return None
         else:
