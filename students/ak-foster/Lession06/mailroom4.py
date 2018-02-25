42d30b6be1789d035e1dd8d69d2a3d22cdd1ec8d
Andrew Foster <akfoster@uw.edu>
2018-02-24 17:47:53 -0800

    Added more unit tests to test_mailroom.py. Refactored mailroom for better test coverage.

--- students/ak-foster/Lession06/mailroom4.py
+++ students/ak-foster/Lession06/mailroom4.py
@@ -4,6 +4,7 @@ from datetime import datetime
 
 donate = {'Merv': [10, 500, 100], 'Linda': [300, 62, 7], 'Larry': [25, 50, 100], 'Sue': [20, 50, 1000]}
 
+
 def sendThankYou(name=False, amount=False):
     """Add new donations to record and displays a thank you note when provided a NAME and donation AMOUNT."""
     if not name:
@@ -15,6 +16,7 @@ def sendThankYou(name=False, amount=False):
 
 
 def recordDonation(name, amount):
+    """Record donation as an entry in the data structure."""
     try:
         donate[name].append(amount)
     except KeyError:
@@ -26,10 +28,9 @@ def thankYouNote(name, amount):
     return f"{name},\n\nThank you for your gift of {amount} dollars. Your donation will be put to good use.\n\n - The Team"
 
 
-
-def promptForName():
-    name = input("\nPlease enter the donor's name. Type 'list' to display all donors or leave blank to quit.\n")
-
+def promptForName(name=False):
+    if not name:
+        name = input("\nPlease enter the donor's name. Type 'list' to display all donors or leave blank to quit.\n")
     if name == "":
         quit()
     elif name == 'list':
@@ -39,9 +40,10 @@ def promptForName():
         return name
 
 
-def promptForAmount():
-    try:
+def promptForAmount(amount=False):
+    if not amount:
         amount = input("\nPlease enter the donation amount or leave blank to quit:\n")
+    try:
         if amount == "":
             quit()
         amount = int(amount)
@@ -49,9 +51,6 @@ def promptForAmount():
     except ValueError:
         print('Donation amount must be a number.')
         promptForAmount()
-
-
-###############
 
 
 def createReport():
@@ -70,42 +69,52 @@ def createReport():
     # format row (left align - column 10 wide, right align - column 20 wide, right align - column 20 wide)
     row = '{:<10}{:>20}{:>20}{:>20}'
 
-    def printLine():
-        line = ['-' * 10, '-' * 20, '-' * 20, '-' * 20]  # line break (multipliers set to match width defined above)
-        print(row.format(*line))
+    line = ['-' * 10, '-' * 20, '-' * 20, '-' * 20]  # line break (multipliers set to match width defined above)
+    line = row.format(*line)
 
-    def printHeader():
-        header = ['Donor Name', 'Total ($)', 'Count', 'Average ($)']
-        print(row.format(*header))
+    header = ['Donor Name', 'Total ($)', 'Count', 'Average ($)']
+    header = row.format(*header)
 
     # print report header with column titles
-    printLine()
-    printHeader()
-    printLine()
+    print(line)
+    print(header)
+    print(line)
 
     # print donation summary sorted
     for record in sortedDonateReport:
         print(row.format(*record))
 
     # print report footer
-    printLine()
+    print(line)
+
 
 def letters():
-    """For all the donors in the donor data structure, generate a thank you letter, and write it to disk as a text file."""
+    """For all the donors, generate a thank you letter and write it to disk as a text file."""
+    filePath = promptForDir()
+    saveLetters()
+    print(f"\nLetters have been saved to the {filePath} directory.\n")
 
-    response = input("Type the directory where these letters should be saved:  ")
-    os.chdir(response)
+
+def promptForDir(filePath=False):
+    "Get directory from user-- create if directory doesn't exist, then change to the directory."
+    if not filePath:
+        filePath = input("Please enter the directory where these letters should be saved:\n")
+    if not os.path.exists(filePath):
+        os.makedirs(filePath)
+    os.chdir(filePath)
+    return filePath
+
+
+def saveLetters():
+    """Write a thank you letter for each donor to a file in the current working directory."""
     for donor, gifts in donate.items():
         with open(donor + '_' + str(datetime.now().date()) + '.txt', 'w+') as f:
             f.write(thankYouNote(donor, sum(gifts)))
-    print(f"\nLetters have been saved to the {response} directory.\n")
-
-
 
 # Prompt with menu
 switcher = {'1': sendThankYou, '2': createReport, '3': letters, '0': quit}
-"""
+
 while True:
     res = input("\nType '1' to send a thank you. \nType '2' to create a report. \nType '3' to send letters. \nType '0' to quit.\n")
     switcher[res]()
-"""
+
--- students/ak-foster/Lession06/test_mailroom.py
+++ students/ak-foster/Lession06/test_mailroom.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python3
 from mailroom4 import *
 
-def test_1():
-    assert thankYouNote('Name', '100') == "Name,\n\nThank you for your gift of 100 dollars. Your donation will be put to good use.\n\n - The Team"
+def test_thankYouNote():
+    assert thankYouNote('Name1', '100') == "Name1,\n\nThank you for your gift of 100 dollars. Your donation will be put to good use.\n\n - The Team"
 
 
-def test_2():
-    recordDonation('Ed', 1000)
-    assert 'Ed' in donate
+def test_recordDonation():
+    recordDonation('Name2', 1000)
+    assert 'Name2' in donate
+
+def test_sendThankYou():
+    sendThankYou('Name3', 200)
+    assert 'Name3' in donate
+
+def test_promptForName():
+    assert 'Name4' == promptForName('Name4')
+
+def test_promptForAmount():
+    assert promptForAmount('100') == 100
+
+def test_
