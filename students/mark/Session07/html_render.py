d15053b43fd100f959aacac9e8d702194f4ceba2
Mark O'Brien <markob@uw.edu>
2018-02-28 20:10:17 -0800

    WIP building program (below name), still broken. 	modified:   html_render.py

--- students/mark/Session07/html_render.py
+++ students/mark/Session07/html_render.py
@@ -7,6 +7,10 @@ A class-based system for rendering html.
 # This is the framework for the base class
 class Element(object):
 
+    self.open_tag=''
+    self.close_tag=''
+    self.empty_tag=''
+
     def __init__(self, content=None):
         self.content = content or ''
 
@@ -14,14 +18,29 @@ class Element(object):
         self.content += new_content
 
     def render(self, file_out, cur_ind=""):
+        self.content = self.open_tag + self.content + self.close_tag
         file_out.write(self.content)
 
 
 class Html():
-    pass
+    def __init__(self):
+        self.open_tag="<html>"
+        self.close_tag="</html>"
+        self.empty_tag="</>"
+        super().__init__(content)
+
 
 class Body(Element):
-    pass
+    def __init__(self):
+        self.open_tag="<body>"
+        self.close_tag="</body>"
+        self.empty_tag="</>"
+        super().__init__(content)
+
 
 class P():
-    pass
+    def __init__(self):
+        self.open_tag="<p>"
+        self.close_tag="</p>"
+        self.empty_tag="</>"
+        super().__init__(content)
