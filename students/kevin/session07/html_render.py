86f50968c0fd8934ec872f7331b4c7ebb1155853
kgeo <kgeo@uw.edu>
2018-03-01 06:45:11 -0800

    Continue working through html_render exercise

--- students/kevin/session07/html_render.py
+++ students/kevin/session07/html_render.py
@@ -5,26 +5,45 @@ A class-based system for rendering html.
 """
 
 # This is the framework for the base class
+# NOTE: need "object" in Python2!
 class Element(object):
-
+    open_tag = ''
+    close_tag = ''
+    empty_tag = ''
+    
     def __init__(self, content=None):
         self.content = content or ''
 
     def append(self, new_content):
-        self.content += new_content
+        try:
+            self.content += new_content
+        except TypeError:
+            # import pdb; pdb.set_trace()
+            # print('ok')
 
     def render(self, file_out, cur_ind=""):
+        self.content = self.open_tag + self.content + self.close_tag
         file_out.write(self.content)
 
         
-# NOTE: need "object" in Python2!
-class Html(object):
-
+class Html(Element):
+    # Will override parent class's __init__
     def __init__(self, content=None):
-        pass
+        self.open_tag = "<html>"
+        self.close_tag = "</html>"
+        super().__init__(content)
 
 
-class Body():
+class Body(Element):
+    def __init__(self, content=None):
+        self.open_tag = "<body>"
+        self.close_tag = "</body>"
+        super().__init__(content)
 
 
-class P():
+class P(Element):
+    def __init__(self, content=None):
+        self.open_tag = "<p>"
+        self.close_tag = "</p>"
+        super().__init__(content)
+    
