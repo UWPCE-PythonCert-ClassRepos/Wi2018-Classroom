973be0652d7955d017d44aba83b60c296eafa9b3
Luke Rider <luke@rider.us>
2018-02-28 18:50:27 -0800

    add session 6 exercises prep for mailroom pytest tests

---
+++ students/luke/s06/test_mailroom.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""
+https://uwpce-pythoncert.github.io/PythonCertDevel/exercises/mailroom.html#exercise-mailroom-testing
+
+Add unit tests for mailroom.py -- using pytest
+"""
+import mailroom as mr
+
+def test_build_thankyou():
+    # Clear donor_list.
+    # Add an entry to donor_list and test.
+
+
+def test_add_donation():
+    # Clear donor_list.
+    # Add an entry to donor_list and test.
+
+
+def test_generate_stats():
+    # Clear donor_list.
+    # Add an entry to donor_list and test.
+
+
