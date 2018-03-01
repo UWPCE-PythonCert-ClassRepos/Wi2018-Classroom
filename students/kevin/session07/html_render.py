0853bd16c19a64a7bfd24b8fb13ea5716b6fd127
kgeo <kgeo@uw.edu>
2018-02-28 19:28:55 -0800

    Initial commit of Session07 files

---
+++ students/kevin/session07/html_render.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+"""
+A class-based system for rendering html.
+"""
+
+# This is the framework for the base class
+class Element(object):
+
+    def __init__(self, content=None):
+        self.content = content or ''
+
+    def append(self, new_content):
+        self.content += new_content
+
+    def render(self, file_out, cur_ind=""):
+        file_out.write(self.content)
