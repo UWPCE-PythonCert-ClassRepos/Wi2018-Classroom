405cac000fc246716bfd21a8a21a371d3cc02867
kgeo <kgeo@uw.edu>
2018-03-01 20:50:51 -0800

    Working Task 2

--- students/kevin/session03/strformat_lab.py
+++ students/kevin/session03/strformat_lab.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python
 
-# Write format string that will take the following tuple and produce the
-# desired output.
-input_tuple = ( 2, 123.4567, 10000, 12345.67 )
-# 'file_002 :   123.46, 1.00e+04, 1.23e+04'
-my_string = 'file_{} :\t{}, {}, {}'.format(input_tuple)
+# Task 1
+my_string = 'file_{:0>3d} :\t{:.2f}, {:.2e}, {:.2e}'.format( 2, 123.4567, 10000, 12345.67 )
+
+print(my_string)
+
+
+# Task 2
+string_tuple = ( 2, 123.4567, 10000, 12345.67 )
+
+my_string2 = (f"file_{string_tuple[0]:0>3d} :\t{string_tuple[1]:.2f}, "
+              f"{string_tuple[2]:.2e}, {string_tuple[3]:.2e}")
+
+print(my_string2)
+
+
+# Task 3
