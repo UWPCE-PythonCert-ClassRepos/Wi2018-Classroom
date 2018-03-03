e5b81affb89bd8611fa3adc836ca9173e8e8aebf
ru57y34nn <mannrusty@gmail.com>
2018-03-03 11:19:14 -0800

    adding Session 07 scripts

---
+++ students/rusty_mann/Session07/html_render.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+A class-based system for rendering html.
+"""
+
+# This is the framework for the base class
+
+
+class Element(object):
+    open_tag = ''
+    close_tag = ''
+    empty_tag = ''
+
+    def __init__(self, content=None):
+        self.content = content or ''
+
+    def append(self, new_content):
+        '''should only append'''
+        try:
+            self.content += new_content
+        except TypeError:
+            pass
+
+    def render(self, file_out, cur_ind=""):
+        self.content = self.open_tag + self.content + self.close_tag
+        import pdb; pdb.set_trace()
+        file_out.write(self.content)
+
+
+class Html(Element):
+    def __init__(self, content=None):
+        self.open_tag = "<html>"
+        self.close_tag = "<html/>"
+        super().__init__(content)
+
+
+class Body(Element):
+    def __init__(self, content=None):
+        self.open_tag = "<body>"
+        self.close_tag = "<body/>"
+        super().__init__(content)
+
+
+class P(Element):
+    def __init__(self, content=None):
+        self.open_tag = "<p>"
+        self.close_tag = "<p/>"
+        super().__init__(content)
+
+
