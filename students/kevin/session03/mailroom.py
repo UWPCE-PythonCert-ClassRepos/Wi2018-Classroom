8d53379a9fa3df53a074941bc1121bb6e07d28fc
kgeo <kgeo@uw.edu>
2018-03-03 16:01:26 -0800

    Move main prompt to main()

--- students/kevin/session03/mailroom.py
+++ students/kevin/session03/mailroom.py
@@ -10,35 +10,68 @@ def add_donation_to_history():
     pass
 
 
+def list_donor_names():
+    """   """
+    pass
+
+
+def main_menu_prompt():
+    """   """
+    response = input("  (1) Send a 'Thank You'\n"
+                     "  (2) Create a Report\n"
+                     "  (3) quit\n"
+                     "  --> ")
+
+    return response
+    
+
+def thank_you_prompt():
+    """   """
+
+    while True:
+        response = ('--> ')
+
+        if reponse in ['list', 'l']:
+            list_donor_names()
+
+
+def report_prompt():
+    """   """
+    pass
+
+
+def main():
+    """ Show main menu, prompting user for selection.  """
+    print('\nWhat would you like to do? (Select one):')
+
+    while True:
+        response = main_menu_prompt()
+
+        if response.lower() in ['1', 'send', 'send a thank you', 'thank you']:
+            thank_you_prompt()
+
+        if response.lower() in ['2', 'create', 'report', 'create a report']:
+            report_prompt()
+
+        if response.lower() in ['3', 'q', 'quit', 'exit']: break
+
+        else:
+            print("Invalid input. Please select one:")
+
+    return
+
+
 
 if __name__ == '__main__':
-
+    # Initialize 5 donors and at least 1 donation for each.
     donors = dict([('William Gates, III', {'donations': (1, 5, 100000000)}),
                    ('Mark Zuckerberg', {'donations': (378000, 5000, 20.01)}),
                    ('Jeff Bezos', {'donations': (29000000, 34000, 709000)}),
                    ('Paul Allen', {'donations': (750000, 513895, 30592.50)}),
                    ('John Ferrell', {'donations': (520000000000)})])
 
-    print('\nWelcome to the Mailroom applicaton.\n\n'
-          'What would you like to do? (Select one):')
-    
-    while True:
-        response = input("  (1) Send a 'Thank You'\n"
-                         "  (2) Create a Report\n"
-                         "  (3) quit\n"
-                         "  --> ")
+    print('\nWelcome to the Mailroom applicaton.')
 
-        if response.lower() in ['1', 'send', 'send a thank you', 'thank you']:
-            pass
-
-        if response.lower() in ['2', 'create', 'report', 'create a report']:
-            pass
-        
-        if response.lower() in ['3', 'q', 'quit', 'exit']: break
-
-        else:
-            print("I didn't understand your input.\n"
-                  "Please select one of the following options:")
-
+    main()
 
     print('\nThank you for using Mailroom. Have a nice day!')
