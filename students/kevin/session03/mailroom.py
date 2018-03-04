99a721f0ae9727f4722aa0a48949cedb4591ac5c
kgeo <kgeo@uw.edu>
2018-03-03 17:34:06 -0800

    Can get in and out of Thank You menu

--- students/kevin/session03/mailroom.py
+++ students/kevin/session03/mailroom.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-def add_new_donor():
+def add_new_donor(name):
     """   """
     pass
 
@@ -11,28 +11,64 @@ def add_donation_to_history():
 
 
 def list_donor_names():
+    """ Print current list of donors to screen  """
+    [print(k) for k in donors.keys()]
+
+
+def hor_bar():
     """   """
-    pass
+    print("-----------------------------------")
 
 
 def main_menu_prompt():
     """   """
-    response = input("  (1) Send a 'Thank You'\n"
-                     "  (2) Create a Report\n"
-                     "  (3) quit\n"
-                     "  --> ")
+    response = input("   (1) Send a 'Thank You'\n"
+                     "   (2) Create a Report\n"
+                     "   (3) quit\n"
+                     "--> ")
 
     return response
+
+
+def direct_from_main_menu(response):
+    """   """
+    
     
 
-def thank_you_prompt():
+def thank_you_prompt(donors):
     """   """
-
+    hor_bar()
+    print("[Send 'Thank You' menu]\n"
+          "Type 'menu' to return to main menu.")
+    
     while True:
-        response = ('--> ')
+        response = input('--> ')
 
-        if reponse in ['list', 'l']:
+        if response in ['list', 'l']:
             list_donor_names()
+
+        # import pdb; pdb.set_trace()
+
+        if response.lower() not in [k.lower() for k in donors.keys()]:
+            # TODO see if I need to return add_new_donor to donors object
+            # here or if global namespace donors will get updated in
+            # add_new_donor function
+            add_new_donor(response)
+
+        if response.lower() in [k.lower() for k in donors.keys()]:
+            new_amount = donation_prompt()
+            donors = add_donation_to_history(new_amount)
+
+        if response in ['m', 'menu']: break
+
+    return
+    # TODO see if I need to return donors to get any updates back into
+    # the donors object in the global namespace
+    # return donors
+
+def donation_prompt():
+    """   """
+    pass
 
 
 def report_prompt():
@@ -42,24 +78,28 @@ def report_prompt():
 
 def main():
     """ Show main menu, prompting user for selection.  """
-    print('\nWhat would you like to do? (Select one):')
-
     while True:
-        response = main_menu_prompt()
+        hor_bar()
+        print('[Main menu]\n'
+              'What would you like to do? (Select one):')
 
-        if response.lower() in ['1', 'send', 'send a thank you', 'thank you']:
-            thank_you_prompt()
+        while True:
+            response = main_menu_prompt()
 
-        if response.lower() in ['2', 'create', 'report', 'create a report']:
-            report_prompt()
+            if response.lower() in ['1', 'send', 'send a thank you', 'thank you']:
+                thank_you_prompt(donors)
+                break
 
-        if response.lower() in ['3', 'q', 'quit', 'exit']: break
+            if response.lower() in ['2', 'create', 'report', 'create a report']:
+                report_prompt()
+                break
 
-        else:
-            print("Invalid input. Please select one:")
+            if response.lower() in ['3', 'q', 'quit', 'exit']: return
+
+            else:
+                print("Invalid input. Please select one:")
 
     return
-
 
 
 if __name__ == '__main__':
