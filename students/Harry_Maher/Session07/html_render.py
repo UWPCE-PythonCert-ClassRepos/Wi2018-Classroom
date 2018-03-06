db903ad8a968737bfac6293a281db9aea54ee712
Harry Maher <harryb.maher@gmail.com>
2018-03-06 15:00:06 -0800

    some pep8 stuff/cleaning up html_render.py

--- students/Harry_Maher/Session07/html_render.py
+++ students/Harry_Maher/Session07/html_render.py
@@ -4,69 +4,69 @@
 A class-based system for rendering html.
 """
 
-# This is the framework for the base class
-class Element(object):
-    # The tag should be a class attr not an instance attr, according to Hettinger
-    # And we need our tag to be html for the 1st test, 
+
+class Element:
+    # The tag should be a class attr not an instance attr
+    # (according to Hettinger).
+    # And we need our tag to be html for the 1st test,
     # the tag gets overwritten in subclasses
-    tag = "html>"
-    def __init__(self, content=None, style=""):
+    tag = "html"
+    
+    def __init__(self, content=None):
         if content:
-            self.content = [content] # A list!
-        else: 
+            self.content = [content]  # A list!
+        else:
             self.content = []
-        if len(style)>1:
-            self.style=style
 
     def append(self, new_content):
         self.content.append(new_content)
 
     # Ideally cur_ind = "" when initially calling render
     def render(self, file_out, cur_ind=""):
-        file_out.write(cur_ind+"<" + self.tag + "\n")
-
+        file_out.write(cur_ind + "<" + self.tag + ">\n")
+        # Recursing cause I was told to.
         for item in self.content:
-            if type(item)== type("s"):
+            if type(item) == type("s"):
                 file_out.write(cur_ind + "\t" + item + '\n')
-            else: #Recursing cause I was told to.
+            else:
                 item.render(file_out, cur_ind+"\t")
-        file_out.write(cur_ind + '</' + self.tag + '\n')
+        file_out.write(cur_ind + '</' + self.tag + '>\n')
 
 
 class Html(Element):
-    # Kept the closing tags. It made sense at the time for some reason, but 
-    # now it doesn't and I'm too lazy to refactor
-    # But it looks like I'll need to to get style in there. ugh.
-    tag = "html>"
+    tag = "html"
+
 
 class P(Element):
-    tag = "p>"
+    tag = "p"
+
 
 class Body(Element):
-    tag = "body>"
+    tag = "body"
+
 
 class Head(Element):
-    tag = "head>"
+    tag = "head"
+
 
 class OneLineTag(Element):
     """
     Create a OneLineTag subclass of Element.
-    It should override the render method, 
-    to render everything 
-    on 
-    one 
+    It should override the render method,
+    to render everything
+    on
+    one
     line
     """
     def render(self, file_out, cur_ind=""):
-        file_out.write(cur_ind+"<" + self.tag)
+        file_out.write(cur_ind + "<" + self.tag + "> ")
         for item in self.content:
-            if type(item)== type("s"):
-                file_out.write(" "+item+" ")
-            else: 
+            if type(item) == type("s"):
+                file_out.write(item)
+            else:
                 item.render(file_out, cur_ind+"\t")
-        file_out.write('</' + self.tag + '\n')
-
+        file_out.write(' </' + self.tag + '>\n')
 
 
 class Title(OneLineTag):
-    tag = "title>"
+    tag = "title"
