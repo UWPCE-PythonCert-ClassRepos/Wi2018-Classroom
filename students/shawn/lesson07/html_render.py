2b1f0bab63aadf3cb382d3bd850355f3b756b86a
shawn.hopkins1@gmail.com <shopkins@seagen.com>
2018-03-07 16:39:36 -0800

    Added functionality for inline styles and pretty print

--- students/shawn/lesson07/html_render.py
+++ students/shawn/lesson07/html_render.py
@@ -14,33 +14,41 @@ class Element():
         self.inline=inline
         self.val=val
 
-    def make_tag(self,indent=0):
+    def add_spaces(self,spaces):
+        return " " * spaces
+
+    def make_tag(self,indent=0,is_break=True):
         self.tag=""
-        self.tag += f"<{ r'/' if self.tag_type==Tag.Close else ''}{self.markup}"
+        self.tag +=   f"{self.add_spaces(indent)}<{ r'/' if self.tag_type==Tag.Close else ''}{self.markup}"
         if self.inline:
             self.tag += " style=\"" + self.inline + "\""
         if self.tag_type==Tag.Span:
             self.tag += f">{self.val}</{self.markup}>"
         else:
             self.tag += f">{self.val if self.val else ''}"
+
+        if is_break:
+            self.tag +="\n"
         return self.tag
-
-
-
 
 class U_list(Element):
 
-    def __init__(self,label,items):
+
+
+    def __init__(self,label,li,indent=0):
+        super(Element,self)
         self.label=label
-        self.items=items
+        self.li=li
+        self.indent=indent
+
 
     def make_list(self):
-        self.ul = Element(Tag.Open,markup="ul",val=self.label + "\n").make_tag()
-        self.ul += '\n'.join([Element(Tag.Span,markup="li",val=i).make_tag() for i in self.items])
-        self.ul += Element(Tag.Close, markup="ul").make_tag()
+        self.ul = Element(Tag.Open,markup="ul",val=self.label ).make_tag(indent=self.indent)
+
+        for i,j in self.li.items():
+            self.ul += (Element(Tag.Span,markup="li",val=j,inline=i).make_tag(indent=self.indent+2) )
+
+        self.ul += Element(Tag.Close, markup="ul",val="").make_tag(indent=self.indent)
+
         return self.ul
-
-
-
-
 
--- students/shawn/lesson07/run_html_render.py
+++ students/shawn/lesson07/run_html_render.py
@@ -2,21 +2,31 @@ import  os
 from html_render import Element,Tag,U_list
 
 
+
+
 with open("mydoc.html",'w') as file:
     file.write(Element(Tag.Open,markup="html").make_tag())
     file.write(Element(Tag.Open,markup="body").make_tag())
 
     file.write(Element(Tag.Span, markup="h1",val="Python Class - Html rendering example").make_tag())
-    file.write(Element(Tag.Open, markup="p").make_tag())
+    file.write(Element(Tag.Open, markup="p").make_tag(indent=2,is_break=False))
+
     file.write(Element(Tag.Span, markup="i"
                        ,inline="text-align:center;"
-                       ,val="Here is a paragraph of text -- there could be more of them, but this is enough to show that we can do some text").make_tag())
+                       ,val="Here is a paragraph of text -- "
+                            "there could be more of them, but this is enough to show that "
+                            "we can do some text").make_tag(is_break=False))
+
     file.write(Element(Tag.Close, markup="p").make_tag())
 
+    file.write(Element(Tag.Span, markup="hr",val="").make_tag(indent=2))
 
-    file.write(U_list(" ",["The first item in a list", "The second item in a list","And this is a link to google"]).make_list())
+    file.write(U_list("",{"color:red":"First item in the list"
+                      ,"font-style:italic":"Second item in the list"
+                      ,"0":"Link to Google"},indent=2).make_list())
 
-    file.write(Element(Tag.Close,markup="html").make_tag())
     file.write(Element(Tag.Close,markup="body").make_tag())
+    file.write(Element(Tag.Close,markup="html").make_tag())
+
 
 
