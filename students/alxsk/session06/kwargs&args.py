e8e54b0ed0d25b627830ddbd84f0ee37ad1e9cb6
Alex Skrivanich <alexandria.is@gmail.com>
2018-03-14 00:10:30 -0700

    kwargs&args.py v02

--- students/alxsk/session06/kwargs&args.py
+++ students/alxsk/session06/kwargs&args.py
@@ -12,7 +12,9 @@ print()
 
 print("passing fore_color purple")
 color_param(fore_color = "purple")
+print()
 
+#same thing as above in a string 
 def string_color(fore_color = "red",back_color = "blue" ,link_color = "yellow", visited_color = "green"):
     print("passing color_param to into a string")
     str_color_param = "The color parameter are {fore_color} fore_color, {back_color} back_color, {link_color} link_color, and {visited_color} visited_color"
@@ -26,4 +28,21 @@ def string_color(fore_color = "red",back_color = "blue" ,link_color = "yellow", 
 string_color()
 
 
+#Generalize paramters in a function using *args and **kwargs
 
+def gen_parameter(*args, **kwargs):
+    return args, kwargs
+
+
+#Add tests here OR pass this file to a test file
+#Call it with a couple different parameters set IOW, write tests that verify that all of the following work as advertised:
+#using just positional arguments:
+#func('red', 'blue', 'yellow', 'chartreuse')
+#using just keyword arguments:
+#func(link_color='red', back_color='blue')
+#using a combination of positional and keyword
+#``func('purple', link_color='red', back_color='blue')
+#using *some_tuple and/or **some_dict
+#regular = ('red', 'blue')
+#links = {'link_color': 'chartreuse'}
+#func(*regular, **links)
