4d9cfc837a95a45f406f2b4ddda1a08ee66bb7b9
Jim Blackburn <jimbb82@uw.edu>
2018-03-13 23:10:49 -0700

    Completed Step 2 with pretty printing.

--- students/jim/Lesson07/html_render.py
+++ students/jim/Lesson07/html_render.py
@@ -1,16 +1,45 @@
 class Element():
+    tag = ""
 
     def __init__(self, content=None):
-        self.tag = "html"
         self.content_items = []
-        if content is str:
+        if content is not None:
             self.content_items.append(content)
 
     def append(self, content):
         self.content_items.append(content)
 
     def render(self, file_out, cur_ind=""):
-        file_out.write(cur_ind+'<'+self.tag+'>'+"\n")
+        # Set indents: content should be more indented than tags
+        tag_indent = cur_ind
+        content_indent = (cur_ind+"    ")
+
+        # Walk through content in the content list. If it's a string,
+        # print it as content. Otherwise, we'll catch the TypeError
+        # and treat it like another Element subclass and recursively
+        # render it.
+        print(tag_indent + self.open_tag())
         for item in self.content_items:
-            file_out.write(cur_ind+"  "+item+"\n")
-        file_out.write(cur_ind+'</'+self.tag+'>')
+            try:
+                print(content_indent + item)
+            except TypeError:
+                item.render(file_out, cur_ind+"    ")
+        print(tag_indent + self.close_tag())
+
+    def open_tag(self):
+        return '<'+self.tag+'>'
+
+    def close_tag(self):
+        return '</'+self.tag+'>'
+
+
+class Html(Element):
+    tag = "html"
+
+
+class Body(Element):
+    tag = "body"
+
+
+class P(Element):
+    tag = "p"
