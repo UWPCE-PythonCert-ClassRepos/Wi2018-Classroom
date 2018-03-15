47f81fe8834f672fd37ae095169717e20c62153c
kgeo <kgeo@uw.edu>
2018-03-14 20:39:23 -0700

    Circle class exercise complete

--- students/kevin/session08/test_ultimate_circle.py
+++ students/kevin/session08/test_ultimate_circle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from ultimate_circle import Circle
+from ultimate_circle import Circle, Sphere
 import math
 import pytest
 
@@ -10,6 +10,9 @@ def my_circle():
 @pytest.fixture
 def my_circle2():
     return Circle(6)
+@pytest.fixture
+def my_sphere():
+    return Sphere(4)
 
 
 def test_radius(my_circle):
@@ -33,6 +36,12 @@ def test_area(my_circle):
 def test_no_set_area(my_circle):
     with pytest.raises(AttributeError):
         my_circle.area = 20
+
+
+def test_alternate_constructor():
+    my_circle = Circle.from_diameter(8)
+    assert my_circle.diameter == 8
+    assert my_circle.radius == 4
 
 
 def test_str(my_circle):
@@ -69,3 +78,30 @@ def test_eq(my_circle, my_circle2):
     c3 = Circle(4)
     assert repr(my_circle == my_circle2) == 'False'
     assert repr(my_circle == c3) == 'True'
+
+
+def test_sphere_str(my_sphere):
+    assert str(my_sphere) == "Sphere with radius: 4.000000"
+
+
+def test_sphere_repr(my_sphere):
+    assert repr(my_sphere) == "Sphere(4)"
+
+
+def test_volume(my_sphere):
+    assert my_sphere.volume == 4 / 3 * math.pi * 4**3
+
+
+def test_sa(my_sphere):
+    assert my_sphere.sa == 4 * math.pi * my_sphere.radius**2
+
+
+def test_sphere_area(my_sphere):
+    with pytest.raises(NotImplementedError):
+        print(my_sphere.area)
+
+
+def test_sphere_alt_constructor():
+    my_sphere = Sphere.from_diameter(8)
+    assert repr(my_sphere) == 'Sphere(4)'
+    assert my_sphere.diameter == 8
