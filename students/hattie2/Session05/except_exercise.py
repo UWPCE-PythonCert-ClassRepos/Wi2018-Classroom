7b3f73e4e4d96b06c1a015f4ebe2f168d1a17ead
Hattie <hspetla@gmail.com>
2018-03-18 13:07:23 -0700

    session4 mailroom complete with dict. and creates files, session05 mailroom has a few list comprehensions and has one example of exception handling

--- students/hattie2/Session05/except_exercise.py
+++ students/hattie2/Session05/except_exercise.py
@@ -14,13 +14,22 @@ from except_test import fun, more_fun, last_fun
 # in that catch block, try again with the second item in the list
 first_try = ['spam', 'cheese', 'mr death']
 
-joke = fun(first_try[0])
+
+try:
+    not_joke = fun(first_try[0])
+except NameError:
+    print(f'There is no joke for {first_try[0]}')
 
 # Here is a try/except block. Add an else that prints not_joke
 try:
     not_joke = fun(first_try[2])
 except SyntaxError:
     print('Run Away!')
+else:
+    print(not_joke)
+
+
+
 
 # What did that do? You can think of else in this context, as well as in
 # loops as meaning: "else if nothing went wrong"
@@ -40,4 +49,12 @@ except SyntaxError:
 
 langs = ['java', 'c', 'python']
 
-more_joke = more_fun(langs[0])
+try:
+    more_joke = more_fun(langs[0])
+except IndexError:
+    next_joke = more_fun(langs[1])
+else:
+    next_joke = more_fun(langs[2])
+finally:
+    next_joke = last_fun()
+
