ca02a934438f0c586de0273312363445cef25593
Luke Rider <luke@rider.us>
2018-04-07 13:36:10 -0700

    add lesson 1 (aka lesson 11) activity and dataset

---
+++ students/luke/s11/activity.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+https://canvas.uw.edu/courses/1188584/assignments/4189634?module_item_id=8331790
+
+Your job, now, is to get artists and song names for for tracks with
+danceability scores over 0.8 and loudness scores below -5.0. In other words,
+quiet yet danceable tracks. Also, these tracks should be sorted in descending
+order by danceability so that the most danceable tracks are up top.
+"""
+
+import pandas as pd
+music = pd.read_csv("featuresdf.csv")
+
+dlist = [x for idx, x
+         in music.sort_values(by='danceability', ascending=False).iterrows()
+         if x.danceability > 0.8 and x.loudness < -5]
+
+print(dlist)
