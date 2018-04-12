a2b5000e55a3ed34542bb7af385d32c7baf072aa
Sophistafunk <jchristo87@gmail.com>
2018-04-11 18:40:32 -0700

    adjusted the code

--- students/jchristo/session11/spotify_songs.py
+++ students/jchristo/session11/spotify_songs.py
@@ -8,7 +8,7 @@ music = pd.read_csv(file_to_copy)
 def get_music(a,n,d,l):
     good_music = []
     for artist,name,danceability,loudness in zip(music.artists,music.name,music.danceability,music.loudness):
-        if danceability > 0.8 and loudness > -5.0:
+        if danceability > 0.8 and loudness < -5.0:
             line = (artist, name, danceability, loudness)
             good_music.append(line)
     return good_music
@@ -20,7 +20,7 @@ good_music = get_music(music.artists,music.name,music.danceability,music.loudnes
 music_key(good_music)
 
 #list comp way from for loop above
-l = [(artist,name,danceability,loudness) for (artist,name,danceability,loudness) in zip(music.artists,music.name,music.danceability,music.loudness) if danceability > 0.8 and loudness > -5.0]
+l = [(artist,name,danceability,loudness) for (artist,name,danceability,loudness) in zip(music.artists,music.name,music.danceability,music.loudness) if danceability > 0.8 and loudness < -5.0]
 
 from timeit import default_timer as timer
 # START MY TIMER
@@ -40,12 +40,13 @@ print(f"{comp_end} list comp")
 """
 Top songs based on criteria:
 Headers: Artist, Song, Danceability, Loudness
-[('Calvin Harris', 'Feels', 0.893, -3.105),
- ('Shawn Mendes', "There's Nothing Holdin' Me Back", 0.857, -4.035),
- ('Rita Ora', 'Your Song', 0.855, -4.093),
- ('Bruno Mars', "That's What I Like", 0.853, -4.961),
- ('Shakira', 'Chantaje', 0.852, -2.9210000000000003),
- ('Travis Scott', 'goosebumps', 0.841, -3.37),
- ('Ed Sheeran', 'Shape of You', 0.825, -3.1830000000000003),
- ('Bruno Mars', '24K Magic', 0.818, -4.282)]
+[('Migos', 'Bad and Boujee (feat. Lil Uzi Vert)', 0.927, -5.313), 
+('Drake', 'Fake Love', 0.927, -9.433), 
+('Kendrick Lamar', 'HUMBLE.', 0.904, -6.8420000000000005), 
+('21 Savage', 'Bank Account', 0.884, -8.228), 
+('Jax Jones', "You Don't Know Me - Radio Edit", 0.8759999999999999, -6.053999999999999), 
+('Liam Payne', 'Strip That Down', 0.8690000000000001, -5.595), 
+('Future', 'Mask Off', 0.833, -8.795), 
+('Zion & Lennox', 'Otra Vez (feat. J Balvin)', 0.8320000000000001, -5.428999999999999), 
+('Drake', 'Passionfruit', 0.809, -11.377)]
 """
