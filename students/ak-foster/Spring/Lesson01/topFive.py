4e2baed85dd123a2c78eeeabf3637e8082290940
Andrew Foster <akfoster@uw.edu>
2018-04-11 18:47:16 -0700

    created topFive.py and completed Lesson01 assignment

--- students/ak-foster/Spring/Lesson01/topFive.py
+++ students/ak-foster/Spring/Lesson01/topFive.py
@@ -6,10 +6,5 @@ music = pd.read_csv("featuresdf.csv")
 # Get danceable songs
 dance_music = [x for x in music.danceability if x > 0.8]
 
-
-# Descending sort
-def sort_dancability(x):
-    return -x[1]
-
 # Get artists and songs with dancability scores above 0.8 and low volume
-print(sorted([(w, x, y, z) for (w, x, y, z) in zip(music.artists, music.danceability, music.loudness, music.name) if x > 0.8 and y < -5.0], key=sort_dancability))
+print(sorted([(w, x, y, z) for (w, x, y, z) in zip(music.artists, music.danceability, music.loudness, music.name) if x > 0.8 and y < -5.0], key=lambda x: -x[1]))
