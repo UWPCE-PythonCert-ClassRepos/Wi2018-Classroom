7b38aff677df9199815637e36275f3a3299032e7
Alex Skrivanich <alexandria.is@gmail.com>
2018-04-17 23:10:42 -0700

    updating music

--- students/alxsk/session10/virtualenv/musical_comprehensions.py
+++ students/alxsk/session10/virtualenv/musical_comprehensions.py
@@ -7,12 +7,5 @@ music = pd.read_csv('music.csv')
 
 print(music[music.loudness > -5].sort_values(by = 'danceability', ascending = False).head(5))
 
-print(music.nlargest('danceability', n = 5,))
 
-#selection = pd.select('music', where 'danceability > 0.8')
-#print(selection)
-#print([x for x  in list(zip(music.danceability,  music.loudness)) if x > 0.8 and x < -5.0])
 
-#panda way
-#music.describe.t 
-#music.query(' ').sorted(' ')
