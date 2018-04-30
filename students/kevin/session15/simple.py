8eef8ec1c2c5fb3f055287a7875d3506a5a936a1
kgeo <kgeo@uw.edu>
2018-04-30 14:23:36 -0700

    Specify logger format

--- students/kevin/session15/simple.py
+++ students/kevin/session15/simple.py
@@ -2,7 +2,16 @@
 import logging
 
 format = "%(asctime)s %(filename)s:%(lineno)-4d %(levelname)s %(message)s"
-logging.basicConfig(level=logging.WARNING, format=format, filename='mylog.log')
+
+# BEGIN NEW STUFF
+formatter = logging.Formatter(format)
+
+file_handler = logging.FileHandler('mylog.log')
+file_handler.setFormatter(formatter)
+
+logger = logging.getLogger()
+logger.addHandler(file_handler)
+# END NEW STUFF
 
 def my_fun(n):
     logging.info(f"Function my_fun called with value {n}")
