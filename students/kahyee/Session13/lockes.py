69b2e5ca97dfde2ccaa94d1df9c7708fe772b3b1
Kahyee Fong <kahyeefong@gmail.com>
2018-05-12 15:10:12 -0700

    adding metamailroom project

--- students/kahyee/Session13/lockes.py
+++ students/kahyee/Session13/lockes.py
@@ -8,14 +8,21 @@ class Locke():
         self.capacity = capacity
     
     def __enter__(self):
-        print("Stopping the pumps./nOpening the doors./nClosing the doors./nRestarting the pumps.")
+        print("Stopping the pumps.\nOpening the doors.")
         return self
     
     def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type is ValueError:
+            print("Too many boats for locke capacity, max number is {}".format(self.capacity))
+        print("Closing the doors.\nRestarting the pumps.\n")
+        return True
         
+    def move_boats_through(self, num_boats):
+        if self.capacity < num_boats:
+            raise ValueError
+        print("Successfully moving {} boats through locke".format(num_boats))
 
-
-if __name__ == ""__main__"":
+if __name__ == "__main__":
     
     small_locke = Locke(5)
     large_locke = Locke(10)
