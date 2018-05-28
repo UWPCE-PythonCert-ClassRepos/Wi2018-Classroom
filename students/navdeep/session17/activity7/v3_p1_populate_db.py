f9de30b5adacfdfd524a069d74f3877effb8592d
Navdeep Gill <navgill18@gmail.com>
2018-05-27 17:13:49 -0700

    Populates the Person class of database

---
+++ students/navdeep/session17/activity7/v3_p1_populate_db.py
@@ -0,0 +1,71 @@
+"""
+    Learning persistence with Peewee and sqlite
+    delete the database file to start over
+        (but running this program does not require it)
+        populate the DB with data
+"""
+
+import logging
+from peewee import *
+from v00_personjob_model import Person
+
+
+def populate_db():
+    """
+    add person data to database
+    """
+
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
+
+    database = SqliteDatabase('personjob.db')
+
+    logger.info('Working with Person class')
+    logger.info('Note how I use constants and a list of tuples as a simple schema')
+    logger.info('Normally you probably will have prompted for this from a user')
+
+    person_name = 0
+    lives_in_town = 1
+    nickname = 2
+
+    people = [
+        ('Andrew', 'Sumner', 'Andy'),
+        ('Peter', 'Seattle', None),
+        ('Susan', 'Boston', 'Beannie'),
+        ('Pam', 'Coventry', 'PJ'),
+        ('Steven', 'Colchester', None),
+    ]
+
+    logger.info('Creating Person records: iterate through the list of tuples')
+    logger.info('Prepare to explain any errors with exceptions')
+    logger.info('and the transaction tells the database to fail on error')
+
+    try:
+        database.connect()
+        database.execute_sql('PRAGMA foreign_keys = ON;')
+        for person in people:
+            with database.transaction():
+                new_person = Person.create(
+                    person_name=person[person_name],
+                    lives_in_town=person[lives_in_town],
+                    nickname=person[nickname])
+                new_person.save()
+                logger.info('Database add successful')
+
+        logger.info('Print the Person records we saved...')
+        for saved_person in Person:
+            logger.info(f'{saved_person.person_name} lives in {saved_person.lives_in_town} ' + \
+                        f'and likes to be known as {saved_person.nickname}')
+
+    except Exception as e:
+        logger.info(f'Error creating = {person[person_name]}')
+        logger.info(e)
+        logger.info('See how the database protects our data')
+
+    finally:
+        logger.info('database closes')
+        database.close()
+
+
+if __name__ == '__main__':
+    populate_db()
