b8d7df8a420e6adb7665877160c98b6bc7330b28
Andrew Foster <akfoster@uw.edu>
2018-06-12 01:30:54 -0700

    Completed Lesson05 assignment.

--- students/ak-foster/Spring/Lesson05/simple.py
+++ students/ak-foster/Spring/Lesson05/simple.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python3
 
+import datetime
 import logging
+from logging.handlers import SysLogHandler
 
 format = "%(asctime)s %(filename)s:%(lineno)-3d %(levelname)s %(message)s"
 
 formatter = logging.Formatter(format)
 
-file_handler = logging.FileHandler('mylog.log')
+file_handler = logging.FileHandler(f'{datetime.date.today()}.log')
 file_handler.setLevel(logging.WARNING)
 file_handler.setFormatter(formatter)
 
 console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.DEBUG)
 console_handler.setFormatter(formatter)
+
+sys_handler = logging.handlers.SysLogHandler()
+sys_handler.setLevel(logging.ERROR)
+sys_handler.setFormatter(logging.Formatter("%(filename)s:%(lineno)-3d %(levelname)s %(message)s"))
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
